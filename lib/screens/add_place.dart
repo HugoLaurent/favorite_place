43594d47179911da241bc248e32beacdc98240e4
HugoLaurent <lauhugopro@gmail.com>
2024-02-08 10:05:17 +0100

    Ajout de la logique d'ajout de place grace au provider

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
-class AddPlaceScreen extends StatefulWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_place/providers/user_places.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
 
   @override
-  State<AddPlaceScreen> createState() {
+  ConsumerState<AddPlaceScreen> createState() {
     return _AddPlaceScreenState();
   }
 }
 
-class _AddPlaceScreenState extends State<AddPlaceScreen> {
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+
+    if (enteredTitle.isEmpty) {
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    Navigator.of(context).pop();
+  }
 
   @override
   void dispose() {
@@ -39,7 +53,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
             ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: _savePlace,
               icon: const Icon(Icons.add),
               label: const Text('Add Place'),
             )
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,26 +1,37 @@
-import 'package:favorite_place/screens/add_place.dart';
-import 'package:favorite_place/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
-class PlacesScreen extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_place/providers/user_places.dart';
+
+import 'package:favorite_place/screens/add_place.dart';
+import 'package:favorite_place/widgets/places_list.dart';
+
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Your Places"), actions: [
-        IconButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) => const AddPlaceScreen(),
-              ),
-            );
-          },
-          icon: const Icon(Icons.add),
-        )
-      ]),
-      body: PlacesList(places: []),
+      appBar: AppBar(
+        title: const Text("Your Places"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: PlacesList(
+        places: userPlaces,
+      ),
     );
   }
 }
