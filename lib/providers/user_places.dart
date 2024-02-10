ba378ac4655a6f54abfbff7d45add88d67c43831
HugoLaurent <lauhugopro@gmail.com>
2024-02-10 10:12:15 +0100

    Ajouter une base de données SQLite pour stocker les lieux de l'utilisateur

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favorite_place/models/place.dart';
 
@@ -16,6 +18,25 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'place.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+        );
+      },
+      version: 1,
+    );
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latidude,
+      'lng': newPlace.location.latidude,
+      'address': newPlace.location.address,
+    });
 
     state = [newPlace, ...state];
   }
